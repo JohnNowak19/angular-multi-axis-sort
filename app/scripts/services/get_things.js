dccf658d02d433d08913970a91b76745aa04e90d
John Nowak <jnowak19@gmail.com>
2015-11-09 16:56:19 -0500

    test for get_many
    need to write test for get_few

--- app/scripts/services/get_things.js
+++ app/scripts/services/get_things.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('myModule', []).factory('myThingsService', function() {
-  var serviceImplementation = {
-  	"get_many": function (callback) {
-      callback({});
-  	}
+  var impl = {};
+
+  impl.many_data = {
+     'Blue Coconut': { size: 'Large', color: 'Blue' },
+     'Pomegrante': { size: 'Large', color: 'Red' },
+     'Cocunut': { size: 'Large', color: 'Green' },
+     'Apple': { size: 'Medium', color: 'Red' },
+     'Banana': { size: 'Medium', color: 'Yellow' },
+     'Blueberry': { size: 'Small', color: 'Blue' },
+     'Cherry': { size: 'Small', color: 'Red' },
+  };
+  impl.get_many = function (callback) {
+    callback(impl.many_data);
   };
 
-  return serviceImplementation;
+  return impl;
 });
--- test/spec/services/get_things.js
+++ test/spec/services/get_things.js
@@ -5,12 +5,24 @@ describe('Service: getThings', function () {
 
   describe('calls to get_many', function(){
     describe('responds in the callback', function(){
-      it('with stuff', function(done) {
+      it('a:1', function(done) {
         var $injector = angular.injector([ 'myModule' ]);
         var myService = $injector.get( 'myThingsService');
 
+        myService.many_data = { 'a': 1 };
         myService.get_many(function (stuff) {
-          expect(stuff).toEqual({});
+          expect(stuff).toEqual({'a': 1});
+          done();
+        });
+      });
+      
+      it('a:2', function(done) {
+        var $injector = angular.injector([ 'myModule' ]);
+        var myService = $injector.get( 'myThingsService');
+
+        myService.many_data = { 'a': 2 };
+        myService.get_many(function (stuff) {
+          expect(stuff).toEqual({'a': 2});
           done();
         });
       });
