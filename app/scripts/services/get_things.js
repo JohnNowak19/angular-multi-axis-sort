3345535542d1d0c7fc22cc38e73e0150f023cac7
John Nowak <jnowak19@gmail.com>
2015-11-10 09:01:19 -0500

    added test for get_few

--- app/scripts/services/get_things.js
+++ app/scripts/services/get_things.js
@@ -4,16 +4,28 @@ angular.module('myModule', []).factory('myThingsService', function() {
   var impl = {};
 
   impl.many_data = {
-     'Blue Coconut': { size: 'Large', color: 'Blue' },
-     'Pomegrante': { size: 'Large', color: 'Red' },
-     'Cocunut': { size: 'Large', color: 'Green' },
-     'Apple': { size: 'Medium', color: 'Red' },
-     'Banana': { size: 'Medium', color: 'Yellow' },
-     'Blueberry': { size: 'Small', color: 'Blue' },
-     'Cherry': { size: 'Small', color: 'Red' },
+    'Blue Coconut': { size: 'Large', color: 'Blue' },
+    'Pomegrante': { size: 'Large', color: 'Red' },
+    'Cocunut': { size: 'Large', color: 'Green' },
+    'Apple': { size: 'Medium', color: 'Red' },
+    'Banana': { size: 'Medium', color: 'Yellow' },
+    'Blueberry': { size: 'Small', color: 'Blue' },
+    'Cherry': { size: 'Small', color: 'Red' },
   };
+  impl.few_data = {
+    'Blue Coconut': { size: 'Large', color: 'Blue' },
+    'Pomegrante': { size: 'Large', color: 'Red' },
+    'Cocunut': { size: 'Large', color: 'Green' },
+    'Cherry': { size: 'Small', color: 'Red' }
+  };
+
   impl.get_many = function (callback) {
     callback(impl.many_data);
+  };
+
+    
+  impl.get_few = function (callback) {
+    callback(impl.few_data);
   };
 
   return impl;
--- test/spec/services/get_things.js
+++ test/spec/services/get_things.js
@@ -10,12 +10,13 @@ describe('Service: getThings', function () {
         var myService = $injector.get( 'myThingsService');
 
         myService.many_data = { 'a': 1 };
-        myService.get_many(function (stuff) {
-          expect(stuff).toEqual({'a': 1});
-          done();
-        });
+        myService.get_many(
+          function (stuff) {
+            expect(stuff).toEqual({'a': 1});
+            done();
+         });
       });
-      
+
       it('a:2', function(done) {
         var $injector = angular.injector([ 'myModule' ]);
         var myService = $injector.get( 'myThingsService');
@@ -26,6 +27,35 @@ describe('Service: getThings', function () {
           done();
         });
       });
+
+
+    });
+  });
+
+  describe('calls to get_few', function(){
+    describe('responds in the callback', function(){
+      it('a:3', function(done) {
+        var $injector = angular.injector([ 'myModule' ]);
+        var myService = $injector.get( 'myThingsService');
+
+        myService.few_data = { 'a': 3 };
+        myService.get_few(function (stuff) {
+          expect(stuff).toEqual({'a': 3});
+          done();
+        });
+      });
+
+      it('a:4', function(done) {
+        var $injector = angular.injector([ 'myModule' ]);
+        var myService = $injector.get( 'myThingsService');
+
+        myService.few_data = { 'a': 4 };
+        myService.get_few(function (stuff) {
+          expect(stuff).toEqual({'a': 4});
+          done();
+        });
+      });
+
     });
   });
 
