a82770752fcdd8f80ab1100cdba83dd8a56bae14
John Nowak <jnowak19@gmail.com>
2015-11-10 09:11:59 -0500

    Refactored tests to take advantage of commonalities between calls to svc

--- test/spec/services/get_things.js
+++ test/spec/services/get_things.js
@@ -1,62 +1,33 @@
 'use strict';
 
+
+
+
 describe('Service: getThings', function () {
   beforeEach(module('angularApp'));
 
-  describe('calls to get_many', function(){
-    describe('responds in the callback', function(){
-      it('a:1', function(done) {
-        var $injector = angular.injector([ 'myModule' ]);
-        var myService = $injector.get( 'myThingsService');
+  var call_types = ['many', 'few'];
+  angular.forEach(call_types, function (call_type) {
+    describe('calls to get_'+call_type, function(){
+      describe('responds in the callback', function(){
+        var tests = [
+          {'a': 1},
+          {'a': 2},
+        ];
+        angular.forEach(tests, function (item) {
+          it(""+item, function(done) {
+            var $injector = angular.injector([ 'myModule' ]);
+            var myService = $injector.get( 'myThingsService');
 
-        myService.many_data = { 'a': 1 };
-        myService.get_many(
-          function (stuff) {
-            expect(stuff).toEqual({'a': 1});
-            done();
-         });
-      });
-
-      it('a:2', function(done) {
-        var $injector = angular.injector([ 'myModule' ]);
-        var myService = $injector.get( 'myThingsService');
-
-        myService.many_data = { 'a': 2 };
-        myService.get_many(function (stuff) {
-          expect(stuff).toEqual({'a': 2});
-          done();
+            myService[call_type+'_data'] = item;
+            myService['get_'+call_type](
+              function (stuff) {
+                expect(stuff).toEqual(item);
+                done();
+             });
+          });
         });
       });
-
-
     });
   });
-
-  describe('calls to get_few', function(){
-    describe('responds in the callback', function(){
-      it('a:3', function(done) {
-        var $injector = angular.injector([ 'myModule' ]);
-        var myService = $injector.get( 'myThingsService');
-
-        myService.few_data = { 'a': 3 };
-        myService.get_few(function (stuff) {
-          expect(stuff).toEqual({'a': 3});
-          done();
-        });
-      });
-
-      it('a:4', function(done) {
-        var $injector = angular.injector([ 'myModule' ]);
-        var myService = $injector.get( 'myThingsService');
-
-        myService.few_data = { 'a': 4 };
-        myService.get_few(function (stuff) {
-          expect(stuff).toEqual({'a': 4});
-          done();
-        });
-      });
-
-    });
-  });
-
 });
